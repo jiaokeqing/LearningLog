96108c95678e9892e08d96c7d58ef40444d72d80
jiaokeqing <13583243451@163.com>
2021-06-23 18:45:06 +0800

    寻找两个正序数组中的中位数

---
+++ src/algorithm/array/MedianOfTwoSortedArrays.java
@@ -0,0 +1,60 @@
+package algorithm.array;
+
+import org.junit.Test;
+
+/**
+ * 寻找两个正序数组中的中位数
+ * <p>
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ *
+ * @example 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ */
+public class MedianOfTwoSortedArrays {
+    /**
+     *时间复杂度O(log(n+m))
+     * 思路：求中位数就是求两个数组第(n+m)/2小的数
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并，如果是奇数，会求两次同样的k
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+        if (len1 > len2) {
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        }
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        } else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+
+    }
+
+    @Test
+    public void test() {
+        int[] a = {1, 3, 10, 15};
+        int[] b = {2};
+        System.out.println(findMedianSortedArrays(a, b));
+    }
+}
